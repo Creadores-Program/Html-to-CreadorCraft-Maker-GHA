274602e08c350f8d019acfa2ba76fc6ac74e9968
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-10-14 08:27:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,86 +9,41 @@ try{
   var github = require('@actions/github');
   var cheerio = require('cheerio');
   var fs = require("fs");
-  var Packager = require("@turbowarp/packager");
 }catch(error){
   console.error(error.stack || error.message);
   core.setFailed(error.stack || error.message);
 }
 console.info(prefix+"Done!");
 console.info(prefix+"Creating CreatorCraft Game...");
-var dirGame = core.getInput("path");
+var dirGameDes = core.getInput("path");
+var dirGame = core.getInput("pathGame");
 try{
-  var scratchG;
   function convertCCG(htms){
     console.info(prefix+"Convert Html to Game by CreatorCraft...");
     let $ = cheerio.load(htms);
     let jsFileCache = "";
-    jsFileCache += "$('pprojr').each(function(){ $('body').append(`<script type='p4-project'>`+$(this).html()+`<`+`/script>`); $(this).remove(); });\n";
     $("script").each(function(){
-      if($(this).attr("type") == "p4-project"){
-        $("body").append("<pprojr>"+$(this).html()+"</pprojr>");
-        $(this).remove();
-        return;
-      }
       jsFileCache += $(this).html() + "\n";
       $(this).remove();
     });
+    if(core.getInput("pathCustomJs") != null && core.getInput("pathCustomJs").trim() != ""){
+      jsFileCache += fs.readFileSync(core.getInput("pathCustomJs"));
+    }
     let cssFileCache = "";
-    cssFileCache += "pprojr{ display: none; visibility: hidden; }\n";
     $("style").each(function(){
       cssFileCache += $(this).html() + "\n";
       $(this).remove();
     });
-    fs.writeFileSync(dirGame+"/main.js", jsFileCache);
-    fs.writeFileSync(dirGame+"/index.css", cssFileCache);
-    fs.writeFileSync(dirGame+"/index.html", $.html());
+    if(core.getInput("pathCustomCSS") != null && core.getInput("pathCustomCSS").trim() != ""){
+      cssFileCache += fs.readFileSync(core.getInput("pathCustomCSS"));
+    }
+    fs.writeFileSync(dirGameDes+"/main.js", jsFileCache);
+    fs.writeFileSync(dirGameDes+"/index.css", cssFileCache);
+    fs.writeFileSync(dirGameDes+"/index.html", $.html());
     console.info(prefix+"Done!");
-    console.info(prefix+"Saved Your project in "+dirGame+"/*");
+    console.info(prefix+"Saved Your project in "+dirGameDes+"/*");
   }
-  async function processStoH(progetD){
-    let callbacK = (type, a, b)=> {};
-    let loadProject = await Packager.loadProject(progetD, callbacK);
-    let packager = new Packager.Packager();
-    packager.project = loadProject;
-    if(core.getInput("pathCustomJs") != ""){
-      packager.options.custom.js = fs.readFileSync(core.getInput("pathCustomJs"));
-    }
-    if(core.getInput("pathCustomCSS") != ""){
-      packager.options.custom.css = fs.readFileSync(core.getInput("pathCustomCSS"));
-    }
-    packager.options.chunks.gamepad = (/true/).test(core.getInput("enableGamepad"));
-    packager.options.loadingScreen.text = core.getInput("loadingText");
-    if(core.getInput("pathExtensions") != ""){
-      packager.options.extensions = JSON.parse(fs.readFileSync(core.getInput("pathExtensions"))).extensions;
-    }
-    packager.options.bakeExtensions = (/true/).test(core.getInput("isBakeExtensions"));
-    packager.options.cloudVariables.specialCloudBehaviors = (/true/).test(core.getInput("specialCloudBehaviors"));
-    let resultPre = await packager.package();
-    convertCCG(resultPre.data);
-  }
-  (async function (){
-    console.info(prefix+"Convert Scratch Game to HTML...");
-    let DataSG;
-    if(core.getInput("id") != ""){
-      scratchG = core.getInput("id");
-      let Dat = await (await fetch("https://trampoline.turbowarp.org/api/projects/"+scratchG)).json();
-      let token = Dat.project_token;
-      processStoH(await (await fetch("https://projects.scratch.mit.edu/"+scratchG+"?token="+token)).arrayBuffer());
-      return;
-    }
-    if(core.getInput("url") != ""){
-      scratchG = core.getInput("url");
-      DataSG = await (await fetch(scratchG)).arrayBuffer();
-      processStoH(DataSG);
-      return;
-    }
-    if(core.getInput("pathGame") != ""){
-      scratchG = core.getInput("pathGame");
-      processStoH(fs.readFileSync(scratchG));
-      return;
-    }
-    throw new Error(prefix+"You need to specify URL, ID or Scratch 3 file to convert!");
-  })();
+  convertCCG(fs.readFileSync(dirGame));
 }catch(error){
     console.error(error.message);
     core.setFailed(error.message);
